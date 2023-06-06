2e7287f781fad4b7abd62a1013ad9a70864a792a
Jon <Jon@steenfamily.com>
2023-06-06 14:58:16 -0500

    Making my own budget app where the callender was

---
+++ Old Calender/callen.js
@@ -0,0 +1,78 @@
+// Get the calendar body element
+const calendarBody = document.getElementById("calendar-body");
+const monthDisplay = document.getElementById("month-display");
+
+// Get the current month and year
+let currentYear = new Date().getFullYear();
+let currentMonth = new Date().getMonth();
+
+// Function to generate the calendar days
+function generateCalendar(year, month) {
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const firstDay = new Date(year, month, 1).getDay();
+
+  // Clear previous calendar days
+  calendarBody.innerHTML = "";
+
+  // Generate calendar days
+  let day = 1;
+  for (let week = 0; day <= daysInMonth; week++) {
+    const row = document.createElement("tr");
+
+    // Generate cells for each day of the week
+    for (let weekday = 0; weekday < 7; weekday++) {
+      if (week === 0 && weekday < firstDay) {
+        // Empty cells before the first day of the month
+        const emptyCell = document.createElement("td");
+        row.appendChild(emptyCell);
+      } else if (day > daysInMonth) {
+        // Empty cells after the last day of the month
+        const emptyCell = document.createElement("td");
+        row.appendChild(emptyCell);
+        day++;
+      } else {
+        // Calendar day cells
+        const cell = document.createElement("td");
+        const number = document.createElement("span");
+        number.classList.add("calendar-date"); // Add the calendar-date class to the span element
+        number.textContent = day;
+        cell.appendChild(number);
+        row.appendChild(cell);
+        day++;
+      }
+    }
+
+    calendarBody.appendChild(row);
+  }
+  
+  // Update month and year display
+  const monthName = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(new Date(year, month));
+  monthDisplay.textContent = `${monthName} ${year}`;
+}
+
+// Generate the initial calendar
+generateCalendar(currentYear, currentMonth);
+
+// Function to go to the previous month
+function goToPreviousMonth() {
+  if (currentMonth === 0) {
+    currentMonth = 11;
+    currentYear--;
+  } else {
+    currentMonth--;
+  }
+
+  generateCalendar(currentYear, currentMonth);
+}
+
+// Function to go to the next month
+function goToNextMonth() {
+  if (currentMonth === 11) {
+    currentMonth = 0;
+    currentYear++;
+  } else {
+    currentMonth++;
+  }
+
+  generateCalendar(currentYear, currentMonth);
+}
--- scripts/callen.js
+++ scripts/callen.js
@@ -1,78 +1,82 @@
-// Get the calendar body element
-const calendarBody = document.getElementById("calendar-body");
-const monthDisplay = document.getElementById("month-display");
+//TO DO
+//Need variables
+//Pay/H
+//Weekly Gross+net, WeeklyExpenses
+//Month Gross+net, MonthExpenses, month remaining
+//Array for
+//OnceMonth Expense [Name, $]
+//Weekly Expense [name, $, DayOfWeek]
 
-// Get the current month and year
-let currentYear = new Date().getFullYear();
-let currentMonth = new Date().getMonth();
+//Net Income calculator
+//  Need week, month
+//Dis month
+//  knows how many of each day there will be
 
-// Function to generate the calendar days
-function generateCalendar(year, month) {
-  const daysInMonth = new Date(year, month + 1, 0).getDate();
-  const firstDay = new Date(year, month, 1).getDay();
+//Variables (let,const)
+let CurrentMonth = new Date().getMonth();
+let CurrentYear = new Date().getFullYear();
+const DisMonth = document.getElementById("DisMonth");
 
-  // Clear previous calendar days
-  calendarBody.innerHTML = "";
+let Pay = 19.86;
 
-  // Generate calendar days
-  let day = 1;
-  for (let week = 0; day <= daysInMonth; week++) {
-    const row = document.createElement("tr");
+let GrosWeek = 0;
+let NetWeek = 0;
+let ExpWeek = 0;
 
-    // Generate cells for each day of the week
-    for (let weekday = 0; weekday < 7; weekday++) {
-      if (week === 0 && weekday < firstDay) {
-        // Empty cells before the first day of the month
-        const emptyCell = document.createElement("td");
-        row.appendChild(emptyCell);
-      } else if (day > daysInMonth) {
-        // Empty cells after the last day of the month
-        const emptyCell = document.createElement("td");
-        row.appendChild(emptyCell);
-        day++;
-      } else {
-        // Calendar day cells
-        const cell = document.createElement("td");
-        const number = document.createElement("span");
-        number.classList.add("calendar-date"); // Add the calendar-date class to the span element
-        number.textContent = day;
-        cell.appendChild(number);
-        row.appendChild(cell);
-        day++;
-      }
-    }
+const GrosWeektd = document.getElementById("WeeklyGross");
+const NetWeektd = document.getElementById("WeeklyNet");
 
-    calendarBody.appendChild(row);
-  }
-  
-  // Update month and year display
-  const monthName = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(new Date(year, month));
-  monthDisplay.textContent = `${monthName} ${year}`;
-}
+let GrosMonth = 0;
+let NetMonth = 0;
 
-// Generate the initial calendar
-generateCalendar(currentYear, currentMonth);
+const GrosMonthtd = document.getElementById("MonthGross");
+const NetMonthtd = document.getElementById("MonthNet");
 
-// Function to go to the previous month
-function goToPreviousMonth() {
-  if (currentMonth === 0) {
-    currentMonth = 11;
-    currentYear--;
-  } else {
-    currentMonth--;
+let ExpMonth = 0;
+let RemMonth = 0;
+
+let PerWeekExp = []; // [name, $, DayOfWeek]
+let PerMonthExp = []; //[Name, $]
+
+
+function GetDayCount(year,month){
+  const WeekdayCount = [0,0,0,0,0,0,0];
+  const date = new Date(year, month, 1);
+
+  while (date.getMonth() === month){
+    const weekday = date.getDay();
+
+    WeekdayCount[weekday]++;
+    date.setDate(date.getDate()+1);
   }
 
-  generateCalendar(currentYear, currentMonth);
+  return WeekdayCount;
 }
 
-// Function to go to the next month
-function goToNextMonth() {
-  if (currentMonth === 11) {
-    currentMonth = 0;
-    currentYear++;
-  } else {
-    currentMonth++;
-  }
 
-  generateCalendar(currentYear, currentMonth);
+function Update(){
+  let days = GetDayCount(CurrentYear ,CurrentMonth);
+  GrosWeek = Pay * 40;
+  NetWeek = GrosWeek - (GrosWeek * 0.1081) -(GrosWeek * 0.0765);
+
+  GrosMonth = GrosWeek * days[5];
+  NetMonth = NetWeek * days[5];
+
+
+  DisMonth.textContent = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(new Date(CurrentYear, CurrentMonth));;
+  GrosWeektd.textContent = `$${GrosWeek.toFixed(2)}`;
+  NetWeektd.textContent = `$${NetWeek.toFixed(2)}`;
+  GrosMonthtd.textContent = `$${GrosMonth.toFixed(2)}`;
+  NetMonthtd.textContent = `$${NetMonth.toFixed(2)}`;
 }
+
+function MonthExpGen(){
+  alert('Gen time')
+}
+
+const AddMonthExp = document.getElementById('AddMonthExp');
+AddMonthExp.addEventListener('click', function() {MonthExpGen();});
+
+
+Update();
+
